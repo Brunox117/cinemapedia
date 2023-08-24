0ac84613ecbb44e23a7bfe14916e02f97fde98f6
Brunox117 <omarjm0910@gmail.com>
2023-08-23 19:48:55 -0600

    Pagina individual de peliculas

---
+++ lib/domain/entities/actor.dart
@@ -0,0 +1,15 @@
+class Actor{
+  final int id;
+  final String name;
+  final String porfilePath;
+  final String? character;
+
+  Actor({
+   required this.id,
+   required this.name,
+   required this.porfilePath,
+   required this.character
+});
+
+
+}
--- lib/infrastructure/datasources/movidedb_datasource.dart
+++ lib/infrastructure/datasources/movidedb_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/movieDB/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/movieDB/moviedb_response.dart';
 import 'package:dio/dio.dart';
 import 'package:cinemapedia/config/constants/environment.dart';
@@ -50,5 +51,16 @@ class MoviedbDatasource extends MoviesDatasource {
     final response =
         await dio.get('/movie/upcoming', queryParameters: {'page': page});
     return _jsonToMovies(response.data);
+  }
+  
+  @override
+  Future<Movie> getMovieById(String id) async {
+    final response =await dio.get('/movie/$id');
+    if(response.statusCode != 200) throw Exception('Movie with id: $id not found');
+
+    final movieDetails = MovieDetails.fromJson(response.data);
+    final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
+
+    return movie;
   }
 }
