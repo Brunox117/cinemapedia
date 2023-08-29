0658d95b603e4ce526b871516ba7d1522cedcbea
Brunox117 <omarjm0910@gmail.com>
2023-08-29 17:30:16 -0600

    Navegacion entre rutas, falta mantener el estado

---
+++ lib/presentation/providers/other/bottom_navigation_selected.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final bottomBarSelected = StateProvider<int>((ref) => 0);
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,17 +1,42 @@
+import 'package:cinemapedia/presentation/providers/other/bottom_navigation_selected.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class CustomBottomNavigation extends StatelessWidget {
+class CustomBottomNavigation extends ConsumerWidget {
   const CustomBottomNavigation({super.key});
 
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(elevation: 0, items: const [
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home_max), label: 'Inicio'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.label_outline), label: 'Categorias'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
-    ]);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int currentIndex = ref.watch(bottomBarSelected);
+    return BottomNavigationBar(
+        elevation: 0,
+        enableFeedback: true,
+        currentIndex: currentIndex,
+        onTap: (value) {
+          onItemTapped(context, value);
+          ref.read(bottomBarSelected.notifier).update((state) => value);
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.label_outline), label: 'Categorias'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
+        ]);
   }
 }
